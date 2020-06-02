f83bb6e6fceade2f3d971baffa34534ca924aa38
andramataloja <andra.mataloja@gmail.com>
2020-06-02 18:09:56 +0300

    maven to gradle

--- to-do-app-backend/todoapp/src/main/java/com/todoapp/todoapp/TodoappApplication.java
+++ to-do-app-backend/todoapp/src/main/java/com/todoapp/todoapp/TodoappApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ToDoAppApplication {
+public class TodoappApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ToDoAppApplication.class, args);
+		SpringApplication.run(TodoappApplication.class, args);
 	}
 
 }
